9b91a4d0662ecab9b23c6cf769ca5c66b99039f8
Mark Ryall <mark@ryall.name>
2011-03-02 20:37:55 +1000

    basic rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Soami::Application.config.secret_token = '5ed249656673f843868206956126807f3f91a79d6e016c0cba71bf953ade074c21bfce2eed25bc081f32c1ba1871a0ab46167598aee8d30d9890d0289114e831'
