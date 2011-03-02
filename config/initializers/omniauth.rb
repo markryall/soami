5dfa32425c25e93cdc409ec34b37a49a2d488181
Mark Ryall <mark@ryall.name>
2011-03-02 21:13:16 +1000

    added one more require to get net/http and net/https initialized correctly

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 require 'omniauth/enterprise'
 require 'net/http'
+require 'net/https'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider 'CAS', :cas_server => ENV['CAS_SERVER']
