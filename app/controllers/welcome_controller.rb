ab630dde154a058d08a69cc9c6cde2ff52bf95e4
Mark Ryall <mark@ryall.name>
2011-03-02 20:52:47 +1000

    added uid to welcome message

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,6 @@ class WelcomeController < ApplicationController
   before_filter :authenticate_cas
 
   def index
-    render :text => '<h1>yes you are</h1>'
+    render :text => "<h1>yes you are #{session[:uid]}</h1>"
   end
 end
